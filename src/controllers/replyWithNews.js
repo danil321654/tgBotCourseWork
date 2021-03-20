d03defebccfa8b5ccfbc12195c4f4c97a9210b7c
danil321654 <danil321654@gmail.com>
2021-03-20 10:29:35 +0300

    timer fix

--- src/controllers/replyWithNews.js
+++ src/controllers/replyWithNews.js
@@ -2,28 +2,30 @@ const axios = require("axios");
 const User = require("../models/user");
 
 module.exports = async ctx => {
-  let existUser = await User.findOne({
-    chatId: ctx.update.message
-      ? ctx.update.message.chat.id
-      : ctx.update.callback_query.message.chat.id
-  });
-  let res = await axios.get(
-    `https://newsapi.org/v2/top-headlines?country=${existUser.country}&apiKey=${process.env.newsApi}`
-  );
-  res = res.data.articles
-    .map(a => ({sort: Math.random(), value: a}))
-    .sort((a, b) => a.sort - b.sort)
-    .map(a => a.value)
-    .slice(3, 6);
+  try {
+    let existUser = await User.findOne({
+      chatId: ctx.update.message
+        ? ctx.update.message.chat.id
+        : ctx.update.callback_query.message.chat.id
+    });
+    let res = await axios.get(
+      `https://newsapi.org/v2/top-headlines?country=${existUser.country}&apiKey=${process.env.newsApi}`
+    );
+    res = res.data.articles
+      .map(a => ({sort: Math.random(), value: a}))
+      .sort((a, b) => a.sort - b.sort)
+      .map(a => a.value)
+      .slice(3, 6);
 
-  await Promise.all(
-    res.map(async el => {
-      await ctx.reply(
-        `${el.title}\n${
-          existUser.language == "RU" ? "Больше информации" : "More info"
-        }: ${el.url}`
-      );
-      return el;
-    })
-  );
+    await Promise.all(
+      res.map(async el => {
+        await ctx.reply(
+          `${el.title}\n${
+            existUser.language == "RU" ? "Больше информации" : "More info"
+          }: ${el.url}`
+        );
+        return el;
+      })
+    );
+  } catch (e) {}
 };
